fb5611afdb3ec5b752c39f9c2a58412f7a276021
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-08 08:49:51 -0700

    My first c script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# !/ bin / bash
+
+/* main is the entry Point and Return: Always 0 (Meaning success) */
+
+main()
+{
+	puts("Programming is like building a multilingual puzzle.\n");
+
+	return (0);
+
+}
