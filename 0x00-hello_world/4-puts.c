2aa7ba685bf9f599a7873292baaf553c7b3387c6
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-08 12:10:16 -0700

    My first c script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-# !/ bin / bash
+#include <stdio.h>
 
-/* main is the entry Point and Return: Always 0 (Meaning success) */
-
-main()
+/**
+*main to print the string that is put in the function
+*
+*The main function prints the text we need to see
+*Return: 0
+*/
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle.\n");
 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
+^C
