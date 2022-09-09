22b571352b366c8c159a116c469257c24ab29b2f
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-09 12:50:07 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-	int c, i, j, k;
+	int c, i, k, j;
 
 	for (c = 48; c <= 57; c++)
 	{
 		for (i = 48; i <= 57; i++)
 		{
-			for (j = 48; j <= 57; j++)
+			for (k = 48; k <= 57; k++)
 			{
-				for (k = 48; k <= 57; k++)
+				for (j = 48; j <= 57; j++)
 				{
 					if (((k + j) > (c + i) && k >= c) || c < k)
 					{
 						putchar(c);
 						putchar(i);
 						putchar(' ');
-						putchar(j);
 						putchar(k);
+						putchar(j);
 
-					if (c + i + j + k == 227 && c == 57)
+					if (c + i + k + j == 227 && c == 57)
 					{
 					break;
 					}
