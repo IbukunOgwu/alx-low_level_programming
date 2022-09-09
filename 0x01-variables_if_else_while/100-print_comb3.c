d93ad34e8bbb58ebbd64ff22f7d08599f1890619
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-09 11:36:39 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the combination of numbers
+ *
+ * Return: Always (Success);
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; 1 <= '9'; i++)
+		{
+			if (c < 1)
+			{
+				putchar(c);
+				putchar(i);
+
+				if (c != '8' | (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
