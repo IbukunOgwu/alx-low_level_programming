86d555279655695a17ea24193a8e1bfdae440b33
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-13 07:42:17 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main-prints alphabets in small letters
+ *
+ * description: lower cases alphabets
+ *
+ * Return: void (success)
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
