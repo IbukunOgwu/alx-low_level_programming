10ca3e391ed6895f183fbfd174066eaefbd73f09
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-13 13:45:22 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,30 +7,24 @@
  */
 void times_table(void)
 {
-int i, j, res;
-for (i = 0; i <= 9; i++)
+int a, b, op;
+for (a = 0; a <= 9; a++)
 {
-for (j = 0; j <= 9; j++)
+_putchar(48);
+for (b = 1; b <= 9; b++)
 {
-res = (i * j);
-if (j != 0)
+op = a * b;
+_putchar(44);
+_putchar(32);
+if (op <= 9)
 {
-_putchar(',');
-_putchar(' ');
-}
-if (res >= 10)
-{
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
-}
-else if (res < 10 && j != 10)
-{
-_putchar(' ');
-_putchar((res % 10) + '0');
+_putchar(32);
+_putchar(op + 48);
 }
 else
 {
-_putchar((res % 10) + '0');
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
 }
 }
 _putchar('\n');
