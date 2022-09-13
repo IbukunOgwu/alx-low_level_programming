5c17cd74559f6eae7287d561abb50d122ae3ae6e
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-13 04:33:23 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Description:This prints _putchar with a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
