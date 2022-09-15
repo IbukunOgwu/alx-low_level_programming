2437970754ab743db19ee0488720253a84f6e1b8
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-15 05:26:17 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers - function that prints the numbers, from 0 to 9,
+ * followed by a new line
  * Return: 0 - 9 and a new line
  */
 
