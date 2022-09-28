d559e79072d857ca049fce11c7315ba7c9892e0a
Ibukun Ogwu <ijogwu@yahoo.com>
2022-09-28 15:34:23 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,37 @@
 #include "main.h"
 
 /**
- * palind_a - this will obtain the lenght of a
- * @a: string
- * @l: the integer where lenght will be counted
+ * _strlen_recursion - this will obtain the lenght of a string
+ * @s: string
  * Return: 1 if success, otherwise if error, -1
  */
-int palind_a(char *a, int l)
+
+int _strlen_recursion(char *s)
 {
-if (*a == 0)
-	return (l - 1);
-return (palind_a(a + 1, l + 1));
+if (*s == '\0')
+return (0);
+else
+return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- * palind_b - this will compare the string against the reverse string
- * @a: string
- * @l: length
+ * comparator - this will compare the string against the reverse string
+ * @s: string
+ * @a: smallest iterator
+ * @b: biggest iterator
  * Return: 1 on success, if error, return -1
  */
 
-int palind_b(char *a, int l)
+int comparator(char *s, int a, int b)
 {
-if (*a != *(a + 1))
-return (0);
-else if (*a == 0)
+if (*(s + a) == *(s + b))
+{
+if (a == b || a == b + 1)
 return (1);
-return (palind_b(a + 1, l - 2));
+else
+return (0 + comparator(s, a + 1, b - 1));
+}
+return (0);
 }
 
 /**
@@ -37,7 +42,8 @@ return (palind_b(a + 1, l - 2));
 
 int is_palindrome(char *s)
 {
-int l;
-l = palind_a(s, 0);
-return (palind_b(s, l));
+if (*s == '\0')
+return (1);
+else
+return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
