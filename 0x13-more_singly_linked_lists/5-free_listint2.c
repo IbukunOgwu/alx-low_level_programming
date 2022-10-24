6cae908c2e906a37ccf7fa5473897d5d5cc06dfb
Ibukun Ogwu <ijogwu@yahoo.com>
2022-10-24 16:04:24 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,19 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *temp;
+struct listint_s *temp = NULL;
+struct listint_s *aux = NULL;
 if (*head == NULL)
 return;
 while (*head)
 {
 temp = *head;
-*head = (*head)->next;
+while (temp != NULL)
+{
+aux = temp->next;
 free(temp);
+temp = aux;
 }
-head = NULL;
+*head = NULL;
+}
 }
