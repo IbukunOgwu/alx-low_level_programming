64458914b9369bc1e241236feb2549aa6c6a39b9
Ibukun Ogwu <ijogwu@yahoo.com>
2022-10-25 13:48:00 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,65 +1,31 @@
 #include"lists.h"
 
 /**
- * free_listp2 - frees a linked list
- * @head: head of a list.
- *
- * Return: no return.
- */
-
-void free_listp2(listp_t **head)
-{
-listp_t *temp;
-listp_t *curr;
-if (head != NULL)
-{
-curr = *head;
-while ((temp = curr) != NULL)
-{
-curr = curr->next;
-free(temp);
-}
-*head = NULL;
-}
-}
-
-/**
- * free_listint_safe - frees a linked list.
- * @h: head of a list that was freed
- * Return: size of the list that was freed.
+ * free_listint_safe - free elements of a list_t list
+ * @h: first element of the list
+ * Return: the size of the list that was free´d
  */
 
 size_t free_listint_safe(listint_t **h)
 {
-size_t nnodes = 0;
-listp_t *hptr, *new, *add;
-listint_t *curr;
-hptr = NULL;
-while (*h != NULL)
+listint_t *temp;
+listint_t *aux;
+size_t i;
+i = 0;
+temp = *h;
+if (h == NULL)
+return (0);
+while (temp != NULL)
 {
-new = malloc(sizeof(listp_t));
-if (new == NULL)
-exit(98);
-new->p = (void *)*h;
-new->next = hptr;
-hptr = new;
-add = hptr;
-while (add->next != NULL)
+i++;
+aux = temp;
+temp = temp->next;
+free(aux);
+if (aux < temp)
 {
-add = add->next;
-if (*h == add->p)
-{
-*h = NULL;
-free_listp2(&hptr);
-return (nnodes);
-}
-}
-curr = *h;
-*h = (*h)->next;
-free(curr);
-nnodes++;
+break;
 }
 *h = NULL;
-free_listp2(&hptr);
-return (nnodes);
+}
+return (i);
 }
